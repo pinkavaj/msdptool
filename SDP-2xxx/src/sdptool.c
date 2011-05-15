5b5383ad460737e72386ec33375b53120b56f095
Jiri Pinkava <jiri.pinkava@vscht.cz>
2011-05-15 16:56:18 +0200

    Add help to SDP commandline tool
    Signed-off-by: Jiri Pinkava <jiri.pinkava@vscht.cz>

--- SDP-2xxx/src/sdptool.c
+++ SDP-2xxx/src/sdptool.c
@@ -4,8 +4,35 @@
 
 void print_help(void)
 {
-        printf("sdptool [-s <serial port>] CMD\n");
-        printf("        CMD := { ... }\n");
+        printf("sdptool [-a <addr>] <io port> <CMD>\n"
+        "        <addr>  - rs485 address of device (1 - 31), defaults to 1\n"
+        "        <io port> - port for comunication with SDP power supply\n"
+        "                win: { COM1 | COM2 | ... }\n"
+        "                Linux: { /dev/ttyS0 | /dev/ttyS1 | ... }\n"
+        "                use \"-\" for std. in and out, output will be send to stderr\n"
+        "        CMD := {\n"
+//        printf("                <sess> - disable front keypad, make PS to |
+//        printf("                <ends> |\n");
+        "                ccom { rs232 | rs485 } |\n"
+        "                gcom |\n"
+        "                gmax |\n"
+        "                govp |\n"
+        "                getd |\n"
+        "                gets |\n"
+        "                getm [{ 1 - 9 }] |\n"
+        "                getp [{ 0 - 19}] |\n"
+        "                gpal |\n"
+        "                volt { 0 - 99.9 } |\n"
+        "                curr { 0 - 9.99 } |\n"
+        "                sovp { 0 - 99.9 } |\n"
+        "                sout { on | off } |\n"
+        "                poww { 1 - 9 } { on | off } |\n"
+        "                prom { 1 - 9 } { 0 - 99.9 } { 0 - 9.99 } |\n"
+        "                prop { 0 - 19 } { 0 - 99.9 } { 0 - 9.99 } { 00:00 - 99:59 } |\n"
+        "                runm { 1 - 9 } |\n"
+        "                runp { 1 - 999 | inf } |\n"
+        "                stop }\n"
+        "        See SDP power supply manual for detailed informations about commands\n");
 }
 
 int main(int argc, char **argv)
